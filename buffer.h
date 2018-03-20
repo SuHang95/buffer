ecaefe2ab0432e4dec32339daea7157f2aa71531
SuHang <suhang95@mail.ustc.edu.cn>
2018-03-20 12:37:39 +0800

    a buffer class which can be use as a stream in net

---
+++ buffer.h
@@ -0,0 +1,363 @@
+#ifndef __BUFFER_H__
+#define __BUFFER_H__
+#include<deque>
+#include<atomic>
+#include<mutex>
+#include<iostream>
+#define defaultsize 0x1000
+
+class buffer {
+public:
+	buffer() {
+		_buffer = new std::deque<char *>();
+		counter = new std::atomic<int>(1);
+		mutex_for_data = new std::mutex;
+		begin = new size_t(-1);
+		end = new size_t(0);
+	}
+	buffer(size_t size) {
+		_buffer = new std::deque<char *>();
+		counter = new std::atomic<int>(1);
+		mutex_for_data = new std::mutex;
+		begin = new size_t(-1);
+		end = new size_t(0);
+
+		if (size<defaultsize) {
+			char *newbuff = new char[defaultsize];
+			_buffer->push_back(newbuff);
+		}
+		else {
+			for (size_t i = 0; i<((size-1) / defaultsize) + 1; i++) {
+				char *newbuff = new char[defaultsize];
+				_buffer->push_back(newbuff);
+			}
+		}
+	}
+	buffer(const buffer& other) {
+		other.counter->fetch_add(1);
+
+		this->_buffer = other._buffer;
+		this->counter = other.counter;
+		this->mutex_for_data = other.mutex_for_data;
+		this->begin = other.begin;
+		this->end = other.end;
+	}
+	buffer& operator=(const buffer& other) {
+		this->counter->fetch_sub(1);
+		other.counter->fetch_add(1);
+
+		if (this->counter->load() == 0) {
+			delete this->counter;
+			mutex_for_data->lock();
+			while (!_buffer->empty()) {
+				char *temp = _buffer->front();
+				delete temp;
+				_buffer->pop_front();
+			}
+			mutex_for_data->unlock();
+			delete _buffer;
+			delete mutex_for_data;
+			delete begin;
+			delete end;
+		}
+
+		this->_buffer = other._buffer;
+		this->counter = other.counter;
+		this->mutex_for_data = other.mutex_for_data;
+		this->begin = other.begin;
+		this->end = other.end;
+	}
+	~buffer() {
+		this->counter->fetch_sub(1);
+		if (this->counter->load() == 0) {
+			std::cout << "Start destroy!";
+			delete this->counter;
+			mutex_for_data->lock();
+			while (!_buffer->empty()) {
+				char *temp = _buffer->front();
+				delete temp;
+				_buffer->pop_front();
+			}
+			mutex_for_data->unlock();
+			delete _buffer;
+			delete mutex_for_data;
+			delete begin;
+			delete end;
+		}
+	}
+
+
+	void push_back_n(char *src, size_t size) {
+		std::lock_guard<std::mutex> protect(*mutex_for_data);
+		if (size == 0) {
+			return;
+		}
+#ifdef _PRINT_DEBUG
+		std::cout << std::endl;
+		if ((*begin) == (size_t)-1) {
+			std::cout << "begin is -1 and end is " << *end << std::endl;
+		}
+		else {
+			std::cout << "begin is " << *begin << " and end is " << *end << std::endl;
+			std::cout << "now size=" << ((*end) - (*begin)) << std::endl;
+			std::cout << "now buffer size= " << _buffer->size() << std::endl;
+		}
+		std::cout << "we will push back " << size << " bytes!" << std::endl;
+#endif
+		if ((*begin) != (size_t)-1) {
+			if ((*begin) >= (*end)) {
+				throw std::logic_error("Buffer end lower than start!");
+			}
+		}
+		else{
+			/*如果buffer为空，因为size不为0，所以现在把begin置0，为确保end正常，
+			也置0，但这事实上是一个不太正常的状态，需要注意*/
+			(*begin) = 0;
+			(*end) = 0;
+		}
+
+		while ((size+(*end))>(_buffer->size()*defaultsize) ) {
+			char *newbuff = new char[defaultsize];
+			_buffer->push_back(newbuff);
+			
+		}
+		for (size_t i = 0; i < size; i++) {
+#ifdef _PRINT_DEBUG
+			if(((i+(*end))/defaultsize)>=_buffer->size()){
+				std::cout<<"Now _buffer size= "<<_buffer->size()<<std::endl;
+				throw std::logic_error("_buffer size is not enough!");
+			}
+			if((*_buffer)[(i + (*end)) / defaultsize]==NULL){
+				throw std::logic_error("Memory Allocate or destroy error!");
+			}
+
+#endif
+			(*_buffer)[(i + (*end)) / defaultsize][(i + (*end)) % defaultsize] = src[i];
+		}
+		(*end) += size;
+#ifdef _PRINT_DEBUG
+		if ((*begin) == (size_t)-1) {
+			std::cout << "begin is -1 and end is " << *end << std::endl;
+		}
+		else {
+			std::cout << "begin is " << *begin << " and end is " << *end << std::endl;
+			std::cout << "now size=" << ((*end) - (*begin)) << std::endl;
+			std::cout << "now buffer size= " << _buffer->size() << std::endl;
+		}
+		std::cout << "we have push back " << size << " bytes!" << std::endl;
+		std::cout << std::endl;
+#endif
+
+	}
+	void pop_back_n(size_t size) {
+		std::lock_guard<std::mutex> protect(*mutex_for_data);
+#ifdef _PRINT_DEBUG
+		std::cout << std::endl;
+		if ((*begin) == (size_t)-1) {
+			std::cout << "begin is -1 and end is " << *end << std::endl;
+		}
+		else {
+			std::cout << "begin is " << *begin << " and end is " << *end << std::endl;
+			std::cout << "now size=" << ((*end) - (*begin)) << std::endl;
+			std::cout << "now buffer size= " << _buffer->size() << std::endl;
+		}
+		std::cout << "we will pop back " << size << " bytes!" << std::endl;
+#endif		
+		if ((*begin) == (size_t)-1) {
+			*end = 0;
+		}
+		//注意，end-begin是队列现在实际的size
+		else if (size > (*end) - (*begin)) {
+			(*begin) = (size_t)-1;
+			(*end) = 0;
+		}
+		else {
+			(*end) -= size;
+		}
+
+		if ((*end) == 0) {
+			size_t now = _buffer->size();
+			while (now > 10) {
+				delete (*_buffer)[0];
+				now--;
+				_buffer->pop_front();
+			}
+		}
+		else {
+			size_t now = _buffer->size();
+			while ((now - ((*end - 1) / defaultsize)) > 10) {
+				delete (*_buffer)[now - 1];
+				now--;
+				_buffer->pop_back();
+			}
+		}
+#ifdef _PRINT_DEBUG
+		if ((*begin) == (size_t)-1) {
+			std::cout << "begin is -1 and end is " << *end << std::endl;
+		}
+		else {
+			std::cout << "begin is " << *begin << " and end is " << *end << std::endl;
+			std::cout << "now size=" << ((*end) - (*begin)) << std::endl;
+			std::cout << "now buffer size= " << _buffer->size() << std::endl;
+		}
+		std::cout << "we have pop back " << size << " bytes!" << std::endl;
+		std::cout << std::endl;
+#endif
+	}
+	void pop_front_n(size_t size) {
+		std::lock_guard<std::mutex> protect(*mutex_for_data);
+#ifdef _PRINT_DEBUG
+		std::cout << std::endl;
+		if ((*begin) == (size_t)-1) {
+			std::cout << "begin is -1 and end is " << *end << std::endl;
+		}
+		else {
+			std::cout << "begin is "<<*begin<<" and end is " << *end << std::endl;
+			std::cout << "now size=" << ((*end) - (*begin)) << std::endl;
+			std::cout << "now buffer size= " << _buffer->size() << std::endl;
+		}
+		std::cout << "we will pop front " << size << " bytes!" << std::endl;
+#endif
+		if ((*begin) == (size_t)-1) {
+			*end = 0;
+		}
+		else if (size > (*end - *begin)) {
+			(*begin) = (size_t)-1;
+			(*end) = 0;
+		}
+		else {
+			(*begin) += size;
+		}
+
+
+		if ((*begin) == (size_t)-1) {
+			while (_buffer->size()> 10) {
+				delete (*_buffer)[0];
+				_buffer->pop_front();
+			}
+		}
+		else {
+			while (((*begin) / defaultsize) > 10) {
+				delete (*_buffer)[0];
+				(*begin) -= defaultsize;
+				(*end) -= defaultsize;
+				_buffer->pop_front();
+			}
+		}
+#ifdef _PRINT_DEBUG
+		if ((*begin) == (size_t)-1) {
+			std::cout << "begin is -1 and end is " << *end << std::endl;
+		}
+		else {
+			std::cout << "begin is " << *begin << " and end is " << *end << std::endl;
+			std::cout << "now size=" << ((*end) - (*begin)) << std::endl;
+			std::cout << "now buffer size= " << _buffer->size() << std::endl;
+		}
+		std::cout << "we have pop front " << size << " bytes!" << std::endl;
+		std::cout << std::endl;
+#endif
+	}
+	void push_front_n(char *src, size_t size) {
+		std::lock_guard<std::mutex> protect(*mutex_for_data);
+		if (size == 0) {
+			return;
+		}
+#ifdef _PRINT_DEBUG
+			std::cout << std::endl;
+			if ((*begin) == (size_t)-1) {
+				std::cout << "begin is -1 and end is " << *end << std::endl;
+			}
+			else {
+				std::cout << "begin is " << *begin << " and end is " << *end << std::endl;
+				std::cout << "now size=" << ((*end) - (*begin)) << std::endl;
+				std::cout << "now buffer size= " << _buffer->size() << std::endl;
+			}
+			std::cout << "we will push front " << size << " bytes!" << std::endl;
+#endif
+		/*如空间不足申请空间，否则直接复制*/
+		if ((*begin) == (size_t)-1) {
+			(*begin)=0;
+			(*end) = 0;
+			while(size>(_buffer->size()*defaultsize)){
+				char *newbuff = new char[defaultsize];
+				_buffer->push_front(newbuff);
+			}
+			(*end)+=size;
+
+		}
+		else {
+			if ((*begin) >= (*end)) {
+				throw std::logic_error("Buffer end lower than start!");
+			}
+			while (size > (*begin)) {
+				char *newbuff = new char[defaultsize];
+				_buffer->push_front(newbuff);
+				(*begin) += defaultsize;
+				(*end) += defaultsize;
+			}
+			(*begin) -= size;
+		}		
+		for (size_t i = 0; i < size; i++) {
+			(*_buffer)[((*begin)+i) / defaultsize][((*begin) + i) % defaultsize] = src[i];
+		}
+#ifdef _PRINT_DEBUG
+		if ((*begin) == (size_t)-1) {
+			
+			std::cout << "begin is -1 and end is " << *end << std::endl;
+		}
+		else {
+			std::cout << "begin is " << *begin << " and end is " << *end << std::endl;
+			std::cout << "now size=" << ((*end) - (*begin)) << std::endl;
+			std::cout << "now buffer size= " << _buffer->size() << std::endl;
+		}
+		std::cout << "we have push front " << size << " bytes!" << std::endl; 
+		std::cout << std::endl;
+#endif	
+	}
+
+
+	char get(size_t n) {
+		std::lock_guard<std::mutex> protect(*mutex_for_data);
+		if (n >= ((*end) - (*begin))|| *begin == (size_t)-1) {
+			throw std::logic_error("Buffer Overflow!");
+		}
+		else {
+			return (*_buffer)[((*begin) + n) / defaultsize][((*begin) + n) % defaultsize];
+		}
+	}
+	void get_n(size_t n,char *dest,size_t size){
+		std::lock_guard<std::mutex> protect(*mutex_for_data);
+		if (((n+size-1) >= ((*end) - (*begin))) || (*begin == (size_t)-1)) {
+			throw std::logic_error("Buffer Overflow!");
+		}
+		for (size_t i = 0; i < size; i++) {
+			dest[i] = (*_buffer)[((*begin) + n + i) / defaultsize][((*begin) + n + i) % defaultsize];
+		}
+	}
+	void set(size_t n,char a) {
+		std::lock_guard<std::mutex> protect(*mutex_for_data);
+		if (n > ((*end) - (*begin))) {
+			throw std::logic_error("Buffer Overflow!");
+		}
+		else {
+			(*_buffer)[((*begin) + n) / defaultsize][((*begin) + n) % defaultsize] = a;
+		}
+	}
+	size_t size() {
+		std::lock_guard<std::mutex> protect(*mutex_for_data);
+		if ((*begin) == size_t(-1)) {
+			return 0;
+		}
+		return ((*end) - (*begin));
+	}
+
+
+private:
+	std::deque<char *>* _buffer;
+	std::atomic<int>* counter;
+	std::mutex* mutex_for_data;
+	size_t* begin;
+	size_t* end;
+};
+
+#endif
---
+++ buffer_test.cpp
@@ -0,0 +1,147 @@
+#include"buffer.h"
+#include<thread>
+#include <cstdlib>  
+#include <cstdio>  
+#include <time.h>
+#include<iostream>
+std::atomic<int> threadnum;
+std::mutex mutex_for_queue;
+#define testbuffsize 69128
+
+
+#ifdef __gnu_linux__
+#include<unistd.h>
+#endif
+
+
+void test(buffer _test);
+
+
+int main() {
+	buffer testbuff;
+	threadnum.store(50); 
+	char *_buff = new char[(0x7521)*sizeof(size_t)];
+	for (size_t i=0,j= 5 * 0x10000; i< 0x7521;i++,j++){
+		((size_t *)_buff)[i] = j;
+	}
+	testbuff.push_back_n(_buff, (0x7521) * sizeof(size_t));
+	delete _buff;
+
+	std::thread newthread(test, testbuff);
+	newthread.detach();
+	while (threadnum.load() != 0) {
+#ifdef _WIN32
+		_sleep(100);
+#else
+		usleep(100);
+#endif
+	}
+	mutex_for_queue.lock();
+	if (testbuff.size() != 0) {
+		for (size_t j = 0; j < testbuff.size() / sizeof(size_t); j++) {
+			size_t i = 0;
+			for (size_t m = 0; m < sizeof(size_t); m++) {
+				((char*)&i)[m] = testbuff.get(j*sizeof(size_t) +m);
+			}
+			std::cout << i<<" ";
+		}
+	}
+	mutex_for_queue.unlock();
+#ifdef _WIN32
+	system("pause");
+#endif
+	return 0;
+
+}
+
+
+inline void datatest(buffer testbuff){
+	srand(time(0));
+	mutex_for_queue.lock();
+	if (rand() % 4 == 0) {
+		if (testbuff.size() != 0)
+			testbuff.pop_front_n((rand() * sizeof(size_t)) % testbuffsize);
+	}
+	else if (rand() % 4 == 1) {
+		if (testbuff.size() != 0) {
+			size_t i = 0;
+			for (size_t j = 0; j < sizeof(size_t); j++) {
+				((char*)&i)[j] = testbuff.get(j);
+			}
+			size_t push_front_size = ((size_t)rand()) % testbuffsize;
+			i -= push_front_size;
+			char *_buff = new char[push_front_size * sizeof(size_t)];
+			for (size_t j = 0; j< push_front_size; j++) {
+				((size_t *)_buff)[j] = i;
+				i++;
+			}
+			testbuff.push_front_n(_buff, push_front_size * sizeof(size_t));
+			delete _buff;
+		}
+		else {
+			size_t push_front_size = ((size_t)rand()) % testbuffsize;
+			char *_buff = new char[push_front_size * sizeof(size_t)];
+
+			for (size_t j = 0; j< push_front_size; j++) {
+				((size_t *)_buff)[j] = j;
+			}
+			testbuff.push_front_n(_buff, push_front_size * sizeof(size_t));
+			delete _buff;
+		}
+	}
+	else if (rand() % 4 == 2) {
+		size_t size = testbuff.size();
+		if (size != 0)
+			testbuff.pop_back_n(rand() % testbuffsize);
+	}
+	else {
+		if (testbuff.size() != 0) {
+			size_t i = 0;
+			for (size_t j = 0; j < sizeof(size_t); j++) {
+				((char*)&i)[j] = testbuff.get(testbuff.size() - sizeof(size_t) + j);
+			}
+			size_t push_back_size = ((size_t)rand()) % testbuffsize;
+			char *_buff = new char[push_back_size * sizeof(size_t)];
+			for (size_t j = 0; j< push_back_size; j++) {
+				((size_t *)_buff)[j] = ++i;
+			}
+			testbuff.push_back_n(_buff, push_back_size * sizeof(size_t));
+			delete _buff;
+		}
+		else {
+			size_t push_back_size = ((size_t)rand()) % testbuffsize;
+			char *_buff = new char[push_back_size * sizeof(size_t)];
+
+			for (size_t j = 0; j< push_back_size; j++) {
+				((size_t *)_buff)[j] = j;
+			}
+			testbuff.push_back_n(_buff, push_back_size * sizeof(size_t));
+			delete _buff;
+		}
+	}
+	mutex_for_queue.unlock();
+}
+
+
+void test1(buffer testbuff) {
+	if (threadnum.load() == 0) {
+		return;
+	}
+	std::thread newthread(test, testbuff);
+	newthread.detach();
+	datatest(testbuff);
+	threadnum.fetch_add(-1);
+}
+
+
+void test(buffer testbuff) {
+
+	if (threadnum.load() == 0) {
+		return;
+	}
+	std::thread newthread(test1, testbuff);
+	newthread.detach();
+	datatest(testbuff);
+	threadnum.fetch_add(-1);
+}
+
